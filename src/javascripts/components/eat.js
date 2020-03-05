557a996161638071411f3a6541a6ed7edaa0f6b0
David Everett <davideverett1989@gmail.com>
2020-03-04 23:05:37 -0600

    Eat logic done.

--- src/javascripts/components/eat.js
+++ src/javascripts/components/eat.js
@@ -1,17 +1,6 @@
 import utils from '../helpers/utils';
 import creatureData from '../helpers/data/creatureData';
 
-
-const moreFull = () => {
-  const myTamagotchi = creatureData.getCreature();
-  myTamagotchi.full += 10;
-};
-
-const lessFull = () => {
-  const myTamagotchi = creatureData.getCreature();
-  myTamagotchi.full -= 3;
-};
-
 const createFeeder = () => {
   const myTamagotchi = creatureData.getCreature();
   let domString = '';
@@ -22,6 +11,24 @@ const createFeeder = () => {
   domString += '<button id="unhealthy">Unhealthy</button>';
   domString += '</div>';
   utils.printToDom('eat', domString);
+  const moreFull = () => {
+    const fullness = myTamagotchi.full;
+    if (fullness < 90) {
+      myTamagotchi.full += 10;
+    } else if (fullness > 89 && fullness < 100) {
+      myTamagotchi.full = 100;
+    }
+    createFeeder();
+  };
+  const lessFull = () => {
+    const fullness = myTamagotchi.full;
+    if (fullness > 3) {
+      myTamagotchi.full -= 3;
+    } else if (fullness < 3 && fullness > 0) {
+      myTamagotchi.full = 0;
+    }
+    createFeeder();
+  };
   $('#healthy').click(moreFull);
   $('#unhealthy').click(lessFull);
 };
