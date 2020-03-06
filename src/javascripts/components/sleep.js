f1f1b55efb77861c1239f07b31e6145d42c2d59b
David Everett <davideverett1989@gmail.com>
2020-03-05 20:22:47 -0600

    Sleep section working.

---
+++ src/javascripts/components/sleep.js
@@ -0,0 +1,34 @@
+import utils from '../helpers/utils';
+import creatureData from '../helpers/data/creatureData';
+
+const createSleepSection = () => {
+  const myTamagotchi = creatureData.getCreature();
+  let domString = '';
+  domString += '<div>';
+  domString += '<h2>Sleep</h2>';
+  domString += `<p>Energy: ${myTamagotchi.energy}</p>`;
+  domString += '<button id="nap">Nap</button>';
+  domString += '<button id="deep-slumber">Deep Slumber</button>';
+  domString += '</div>';
+  utils.printToDom('sleep', domString);
+  const nap = () => {
+    if (myTamagotchi.energy < 50) {
+      myTamagotchi.energy += 50;
+    } else if (myTamagotchi.energy > 49 && myTamagotchi.energy < 100) {
+      myTamagotchi.energy = 100;
+    }
+    createSleepSection();
+  };
+  const deepSlumber = () => {
+    if (myTamagotchi.energy < 40) {
+      myTamagotchi.energy += 60;
+    } else if (myTamagotchi.energy > 39 && myTamagotchi.energy < 100) {
+      myTamagotchi.energy = 100;
+    }
+    createSleepSection();
+  };
+  $('#nap').click(nap);
+  $('#deep-slumber').click(deepSlumber);
+};
+
+export default { createSleepSection };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,11 +2,13 @@ import '../styles/main.scss';
 import eat from './components/eat';
 import play from './components/play';
 import fight from './components/fight';
+import sleep from './components/sleep';
 
 const init = () => {
   eat.createFeeder();
   play.createPlaySection();
   fight.createFightSection();
+  sleep.createSleepSection();
 };
 
 init();
